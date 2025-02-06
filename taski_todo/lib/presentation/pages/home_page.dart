ece5108aefe723848db6b941c5bb45a91e0e34dd
Nicolas Vitorino <nicolasvvitorino@gmail.com>
2025-02-06 00:46:14 -0300

    inserido fonte, imagens e começo da criação da home com barra inferior

--- taski_todo/lib/presentation/pages/home_page.dart
+++ taski_todo/lib/presentation/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:taski_todo/logic/blocs/task_bloc/task_bloc.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,11 +10,111 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<TaskBloc>().add(GetAllTasks());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: SvgPicture.asset('assets/logo_title_appbar.svg'),
+        leading: Image.asset('assets/logo.jpg'),
+        actions: [
+          Image.asset('assets/user.jpg'),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: [
+          BottomNavigationBarItem(
+              icon: Image.asset('assets/todo_bottom_bar.jpg'), label: ''),
+          BottomNavigationBarItem(
+              icon: Image.asset('assets/create_bottom_bar.jpg'), label: ''),
+          BottomNavigationBarItem(
+              icon: Image.asset('assets/search_bottom_bar.jpg'), label: ''),
+          BottomNavigationBarItem(
+              icon: Image.asset('assets/done_bottom_bar.jpg'), label: ''),
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            RichText(
+              text: const TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'Welcome, ',
+                    style: TextStyle(
+                      fontFamily: 'Urbanist',
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFF3F3D56),
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'John',
+                    style: TextStyle(
+                      fontFamily: 'Urbanist',
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFF007FFF),
+                    ),
+                  ),
+                  TextSpan(
+                    text: '.',
+                    style: TextStyle(
+                      fontFamily: 'Urbanist',
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFF3F3D56),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            BlocBuilder<TaskBloc, TaskState>(
+              builder: (context, state) {
+                if (state is TaskLoaded) {
+                  int tasksQuantity =
+                      state.tasks.length; // Obtém o número de tarefas
+                  return Text(
+                    'You\'ve got $tasksQuantity to do',
+                    style: const TextStyle(
+                      fontFamily: 'Urbanist',
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF8D9CB8),
+                    ),
+                  );
+                } else if (state is TaskLoading) {
+                  return const CircularProgressIndicator(); // Mostra um loader enquanto carrega
+                } else {
+                  return const Text(
+                    'No tasks found.',
+                    style: TextStyle(
+                      fontFamily: 'Urbanist',
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF8D9CB8),
+                    ),
+                  ); // Caso não tenha tarefas
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ taski_todo/lib/presentation/widgets/task_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class TaskWidget extends StatelessWidget {
+  const TaskWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
